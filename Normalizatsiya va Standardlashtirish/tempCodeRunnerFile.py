85b878fd76b1f3d783ff5b54a555e4fb87d192dd
SardorNigmatov <sardornigmatov2003@gmail.com>
2024-02-09 21:27:00 +0500

    add

---
+++ Normalizatsiya va Standardlashtirish/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+rint("Normalizatsiya natijasi:")
+# for row in normalized_data:
+#     formatted_row = " ".join("{0:.3f}".format(val) for val in row)
+#     print(formatted_row)
+
+# # Standartlashtirish
+# standardized_data = my_standardize(data)
+
+# print("\nStandartlashtiri
